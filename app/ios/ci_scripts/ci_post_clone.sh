9a52a2dad15e5a7222d09377da1b14c468851623
Michael Rittmeister <michael@rittmeister.in>
2023-05-23 11:31:43 +0200

    Add XCloud ci scripts
    Add XCloud ci scripts

--- app/ios/ci_scripts/ci_post_clone.sh
+++ app/ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 brew install cocoapods
-brew install openjdk@20
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 19.0.1-tem
 
 # cd into actual project root
 cd ../../../
