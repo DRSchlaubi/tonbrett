d084c18b4b53cb5ebace0ae676c431f60ff9ce6d
Michael Rittmeister <michael@rittmeister.in>
2023-05-24 11:42:52 +0200

    Fix AuthorizationServer not stopping correctly

--- app/desktop/src/main/kotlin/AuthorizationServer.kt
+++ app/desktop/src/main/kotlin/AuthorizationServer.kt
@@ -2,24 +2,28 @@ package dev.schlaubi.tonbrett.app.desktop
 
 import dev.schlaubi.tonbrett.app.api.Config
 import dev.schlaubi.tonbrett.app.api.saveConfig
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.*
 
-fun startAuthorizationServer(reAuthorize: Boolean, onAuth: () -> Unit) =
-    embeddedServer(Netty, port = 12548, module = { authModule(onAuth) }).start(wait = !reAuthorize)
+fun startAuthorizationServer(reAuthorize: Boolean, onAuth: () -> Unit){
+    val scope = CoroutineScope(Dispatchers.Default)
+    scope.embeddedServer(Netty, port = 12548, module = { authModule(onAuth, scope) }).start(wait = !reAuthorize).stopServerOnCancellation()
+}
 
-fun Application.authModule(onAuth: () -> Unit) {
+fun Application.authModule(onAuth: () -> Unit, scope: CoroutineScope) {
     routing {
         get("login") {
             val token = call.parameters["token"] ?: throw BadRequestException("Missing token")
 
             saveConfig(Config(token))
-            call.respond("You can close this tab now")
-            (application.environment as ApplicationEngineEnvironment).stop()
+            call.respond(HttpStatusCode.Gone,"You can close this tab now")
+            scope.cancel()
             onAuth()
         }
     }
