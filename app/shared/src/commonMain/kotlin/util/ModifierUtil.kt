4fcf631ae27a9c43f5b74b66308e5f87cf621a89
Michael Rittmeister <michael@rittmeister.in>
2023-05-29 14:23:08 +0200

    Fix compiler error

--- app/shared/src/commonMain/kotlin/util/ModifierUtil.kt
+++ app/shared/src/commonMain/kotlin/util/ModifierUtil.kt
@@ -1,13 +1,15 @@
 package dev.schlaubi.tonbrett.app.util
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 /**
  * Applies [modifier] if [condition] is true.
  */
+@Composable
 fun Modifier.conditional(
     condition: Boolean,
-    modifier: (Modifier).() -> Modifier
+    modifier: @Composable ((Modifier).() -> Modifier)
 ): Modifier {
     return if (condition) {
         modifier()
