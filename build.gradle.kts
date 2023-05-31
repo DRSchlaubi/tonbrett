606378ad317cc4f426fbbbc8d58d608154406887
Michael Rittmeister <michael@rittmeister.in>
2023-05-31 13:30:28 +0200

    [ci skip] Reformat build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
- plugins {
+plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.library) apply false
-     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
- }
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+}
 
 allprojects {
     group = "dev.schlaubi.tonbrett"
