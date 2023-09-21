5c2401f6002655d3e16c6a3048b81da967c58350
Michael Rittmeister <michael@rittmeister.in>
2023-09-21 14:20:06 +0200

    Fix msix signing

--- app/desktop/msix/update_msix_version.ps1
+++ app/desktop/msix/update_msix_version.ps1
@@ -1,6 +1,5 @@
 param (
-    [Parameter(Mandatory=$true)][String]$Version,
-    [Parameter(Mandatory=$false)][String]$IsMsix = "false"
+    [Parameter(Mandatory=$true)][string]$Version
 )
 
 $file = 'appxmanifest.xml'
@@ -9,14 +8,5 @@ $xml.Load($file)
 
 $identity = $xml.GetElementsByTagName("Identity")[0]
 $identity.SetAttribute("Version", $Version)
-
-if ($IsMsix.Equals("true"))
-{
-    $identity.SetAttribute("Name", "11839Schlaubi.dev.Tonbrett")
-    $identity.SetAttribute("Publisher", "CN=4C05965C-D683-4386-B189-07E2E08CAA6C")
-
-    $displayName = $xml.GetElementsByTagName("PublisherDisplayName")[0]
-    $displayName.InnerText = "Schlaubi.dev"
-}
 
 $xml.Save($file)
