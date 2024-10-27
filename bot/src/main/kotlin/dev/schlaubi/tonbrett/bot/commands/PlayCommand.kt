3e29bd2e059dd47b8f17b3fdd8180b437a8259aa
Michael Rittmeister <michael@rittmeister.in>
2024-10-27 22:20:10 +0100

    Make play command a sub command

--- bot/src/main/kotlin/dev/schlaubi/tonbrett/bot/commands/PlayCommand.kt
+++ bot/src/main/kotlin/dev/schlaubi/tonbrett/bot/commands/PlayCommand.kt
@@ -1,12 +1,10 @@
 package dev.schlaubi.tonbrett.bot.commands
 
 import com.kotlindiscord.kord.extensions.modules.unsafe.annotations.UnsafeAPI
-import com.kotlindiscord.kord.extensions.modules.unsafe.extensions.unsafeSlashCommand
 import dev.schlaubi.mikbot.plugin.api.module.SubCommandModule
 
 @OptIn(UnsafeAPI::class)
-suspend fun SubCommandModule.playCommand() = unsafeSlashCommand {
-    name = "play"
+fun SubCommandModule.playCommand() = unsafeSubCommand("play") {
     description = "commands.play.description"
 
     action {
